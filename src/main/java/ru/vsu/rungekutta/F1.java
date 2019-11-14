df3e7a27faaa23e4ba7228bcb3ed3f8b876da936
alsm0918 <aleksandr.s.smirnov@netcracker.com>
2019-11-14 16:29:20 +0300

    qwe

---
+++ src/main/java/ru/vsu/rungekutta/F1.java
@@ -0,0 +1,8 @@
+package ru.vsu.rungekutta;
+
+public class F1 implements Function {
+    @Override
+    public double solve(Integer... vars) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/vsu/rungekutta/RungeKutta.java
@@ -0,0 +1,5 @@
+package ru.vsu.rungekutta;
+
+public class RungeKutta {
+
+}
