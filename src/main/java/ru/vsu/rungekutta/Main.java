2d41dfe164f4f1523c05eac95ca74d5a183ad21d
alsm0918 <aleksandr.s.smirnov@netcracker.com>
2019-11-14 15:09:45 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/rungekutta/Main.java
@@ -0,0 +1,33 @@
+package ru.vsu.rungekutta;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    void onActionDrawCircle(ActionEvent event) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
