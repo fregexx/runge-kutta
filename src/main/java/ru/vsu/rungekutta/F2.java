dd5d26c63312a0656841ba0b22f777586475b749
alsm0918 <aleksandr.s.smirnov@netcracker.com>
2019-11-15 15:17:02 +0300

    qwe

--- src/main/java/ru/vsu/rungekutta/F2.java
+++ src/main/java/ru/vsu/rungekutta/F2.java
@@ -1,12 +1,12 @@
 package ru.vsu.rungekutta;
 
-public class F1 implements Function {
+public class F2 implements Function {
     @Override
     public double solve(Double t, Double... vars) {
-        return vars[0] + Math.exp(t);
+        return vars[0] + vars[1] - Math.exp(t);
     }
 
     public double exactValue(Double t, Double vars) {
-        return Math.exp(t) * t;
+        return Math.exp(t) * (0.5 * t * t - t);
     }
 }
--- src/main/java/ru/vsu/rungekutta/RungeKutta.java
+++ src/main/java/ru/vsu/rungekutta/RungeKutta.java
@@ -1,5 +1,11 @@
 package ru.vsu.rungekutta;
 
 public class RungeKutta {
+    double approxY(double y, double h, double[] k) {
+        return y + h * (k[0] + 2 * k[1] + 2 * k[2] + k[3]) / 6;
+    }
 
+    double[] getK(double h, ){
+
+    }
 }
